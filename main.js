dc7324d5ae52a582e93a903d7b54d36d3e99e4bd
vickynedelcheva <36950317+vickynedelcheva@users.noreply.github.com>
2020-01-28 13:20:55 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+const nav = document.querySelector(".nav-links");
+const burger = document.querySelector(".burger");
+const links = document.querySelector("a");
+
+burger.addEventListener("click", () => {
+  nav.classList.toggle("nav-open");
+  burger.classList.toggle("toggle");
+});
+
+links.forEach(link => {
+  link.addEventListener("click", () => {
+    nav.classList.toggle("nav-open");
+    burger.classList.toggle("toggle");
+  });
+});
